ab010fd42280d542a2f7c9aa7bbc4837cb5ea375
Marcello DeSales <marcello.desales@gmail.com>
2022-04-12 21:23:44 -0700

    :necktie: Add watcher, parser, data service client in driver
    the service.json drives the execution. The process watches the files in the fs,
    while the parser is executed after an event occurs: bootstrap list or fs events.
    When parsed, the set of wallets and their respective transactions are processed
    by the Data Service client.

--- services/bitcoin-transaction-files-watcher/service.js
+++ services/bitcoin-transaction-files-watcher/service.js
@@ -1,10 +1,17 @@
 "use strict";
 
+// Load the config instance based on the environment
 const configInstance = require("./kraken/platform/util/config")
 
+// Load the classes
 const KrakenTransactionsFileWatcher = require("./kraken/platform/blockchain/bitcoin/transactions/KrakenTransactionsFileWatcher")
-const KrakenTransactionsByAddressParser = require("./kraken/platform/blockchain/bitcoin/transactions/KrakenTransactionsByAddressParser")
+const KrakenValidDepositsByAddressParser = require("./kraken/platform/blockchain/bitcoin/transactions/KrakenValidDepositsByAddressParser")
+const KrakenTransactionsDataServiceClient = require("./kraken/platform/blockchain/bitcoin/transactions/KrakenTransactionsDataServiceClient")
 
-const transactionsByAddressParser = new KrakenTransactionsByAddressParser({config: configInstance});
-new KrakenTransactionsFileWatcher({ config: configInstance, transactionsParser: transactionsByAddressParser});
+// Provide the file transactions watcher
+new KrakenTransactionsFileWatcher({
+    config: configInstance,
+    transactionsParser: new KrakenValidDepositsByAddressParser({config: configInstance}),
+    dataServiceClient:  new KrakenTransactionsDataServiceClient({config: configInstance})
+});
 
